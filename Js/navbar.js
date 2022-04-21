ab2470c2e0c65e52713baaeafe76bbf925fc7ac8
BrianSFarfan <74477887+BrianSFarfan@users.noreply.github.com>
2022-04-21 01:48:43 -0300

    Delete Js directory

--- Js/navbar.js
+++
@@ -1,9 +0,0 @@
-var nav = document.querySelector('nav');
-
-window.addEventListener('scroll', function () {
-  if (window.pageYOffset > 660) {
-    nav.classList.add('bg-info', 'shadow');
-  } else {
-    nav.classList.remove('bg-info', 'shadow');
-  }
-});
